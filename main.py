b1a3a8965d83c9a142ac5d147fac1aac4bc06552
rafaelMachado1891 <rafadmn1@gmail.com>
2024-08-04 14:49:42 -0300

    conclusao aula 16

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class Hero(SQLModel, table=True):
@@ -8,3 +8,11 @@ class Hero(SQLModel, table=True):
     name: str
     secret_name: str
     age: Optional[int] = None
+
+
+engine = create_engine("sqlite:///database.db")
+
+with Session(engine) as session:
+    statement = select(Hero).where(Hero.name == "Spider-Boy")
+    hero = session.exec(statement).first()
+    print(hero)
